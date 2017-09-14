12ea63071a4ead697f355c3fd06b8e6654ab2310
Zach Siegel <zachsiegel41@gmail.com>
2017-09-14 02:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const app = "I don't do much."
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+]
+
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph')
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob')
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(kittens, name) {
+  return [...kittens, "Broom"]
+}
+
+function prependKitten(kittens, name) {
+  return ["Arnold", ...kittens]
+}
+
+function removeLastKitten(kittens, name) {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(kittens, name) {
+  return kittens.slice(1)
+}
